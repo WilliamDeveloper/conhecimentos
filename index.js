c7a743e092588639e49875ba3e58dad1d13204b5
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-24 13:33:09 -0300

    #

--- index.js
+++ index.js
@@ -8,32 +8,24 @@ var dados = [
         ]
     },
     {
-        titulo: 'Google Uteis2',
+        titulo: '==REDE==',
         listaLinks:[
-            {linkUrl:'#', linkDescricao:'a'},
+            {linkUrl:'https://www.charlesproxy.com/', linkDescricao:'monitoramento rede charles proxy 4.2.8'},
             {linkUrl:'#', linkDescricao:'b'},
             {linkUrl:'#', linkDescricao:'c'},
+        ]
+    },
+    {
+        titulo: '==Metadata==',
+        listaLinks:[
+            {linkUrl:'https://www.elevenpaths.com/labstools/foca/index.html', linkDescricao:'foca pesquisa e baixa arquivos de um site para explorar os metadados'},
+            {linkUrl:'https://exiftool.org/', linkDescricao:'ler metadata do arquivo'},
+            {linkUrl:'https://www.google.com.br/maps/@-29.9886319,-51.1062833,15z', linkDescricao:'cordenadas google'},
         ]
     }
 ]
 
-//
-// <h2>==REDE==</h2>
-//     <a class="badge badge-primary" href="https://www.charlesproxy.com/" >monitoramento rede charles proxy 4.2.8</a>
-//
-//
-// <h2>==Metadata==</h2>
-//     <a class="badge badge-primary" href="https://www.elevenpaths.com/labstools/foca/index.html" >monitoramento rede charles proxy 4.2.8</a>
-// <p> foca pesquisa e baixa arquivos de um site para explorar os metadados -
-// <p> ler metadata do arquivo - https://exiftool.org/
-// <p> cordenadas google - https://www.google.com.br/maps/@-29.9886319,-51.1062833,15z
-//
-// <h2>==info navegador==</h2>
-// <p> informacao sobre quem ta usando o navegador - https://www.navegg.com/politica-de-privacidade/seu-perfil/
-// <p> informacao ID unica do browser na internet - https://amiunique.org/
-//
-// <h2>Google Uteis</h2>
-// <a class="badge badge-primary" href="https://myaccount.google.com/lesssecureapps" >ativar seguranca de apps na conta google</a>
+
 
 
 $(document).ready(function () {
