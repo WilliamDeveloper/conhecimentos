55d44628ae4d6278108fa08a6a2daa673fc0439b
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-24 13:50:46 -0300

    #

---
+++ dados.js
@@ -0,0 +1,39 @@
+var assuntos = [
+    {
+        titulo: 'Google Uteis',
+        listaLinks:[
+            {url:'#', descricao:'a'},
+            {url:'#', descricao:'b'},
+            {url:'#', descricao:'c'},
+        ]
+    },
+    {
+        titulo: 'REDE',
+        listaLinks:[
+            {url:'https://www.charlesproxy.com/', descricao:'monitoramento rede charles proxy 4.2.8'},
+
+        ]
+    },
+    {
+        titulo: 'Metadata',
+        listaLinks:[
+            {url:'https://www.elevenpaths.com/labstools/foca/index.html', descricao:'foca pesquisa e baixa arquivos de um site para explorar os metadados'},
+            {url:'https://exiftool.org/', descricao:'ler metadata do arquivo'},
+            {url:'https://www.google.com.br/maps/@-29.9886319,-51.1062833,15z', descricao:'cordenadas google'},
+        ]
+    },
+    {
+        titulo: 'info Navegador',
+        listaLinks:[
+            {url:'https://www.navegg.com/politica-de-privacidade/seu-perfil/', descricao:'informacao sobre quem ta usando o navegador'},
+            {url:'https://amiunique.org/', descricao:'informacao ID unica do browser na internet'},
+        ]
+    },
+    {
+        titulo: 'info Navegador',
+        listaLinks:[
+            {url:'https://www.navegg.com/politica-de-privacidade/seu-perfil/', descricao:'informacao sobre quem ta usando o navegador'},
+            {url:'https://amiunique.org/', descricao:'informacao ID unica do browser na internet'},
+        ]
+    },
+]
