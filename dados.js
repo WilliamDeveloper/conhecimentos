1c9b1ada724511c72b1c091ff9275306ec38ba42
William Goulart Pacheco <gtaskwill@gmail.com>
2021-04-24 15:18:10 -0300

    #

--- dados.js
+++ dados.js
@@ -15,7 +15,7 @@ var assuntos = [
     {
         titulo: 'Metadata',
         listaLinks:[
-            {url:'https://www.elevenpaths.com/labstools/foca/index.html', descricao:'foca pesquisa e baixa arquivos de um site para explorar os metadados'},
+            {url:'https://www.elevenpaths.com/labstools/foca/index.html', descricao:'foca pesquisa e baixa arquivos de um site para explorar os metadados.'},
             {url:'https://exiftool.org/', descricao:'ler metadata do arquivo'},
             {url:'https://www.google.com.br/maps/@-29.9886319,-51.1062833,15z', descricao:'cordenadas google'},
         ]
